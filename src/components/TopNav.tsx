2f35d1695c4be5f59532f491bfc6bf286ec9f445
Steven Wang <wnste@amazon.com>
2026-01-22 19:33:42 -0800

    Update nav and home page

--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
-const TopNav: React.FC = () => (
-  <nav className="top-nav">
-    <ul className="top-nav-links">
-      <li><NavLink to="/">Home</NavLink></li>
-      <li><NavLink to="/cafe">Cafe</NavLink></li>
-      <li><NavLink to="/cooking">Cooking</NavLink></li>
-      <li><NavLink to="/travel">Travel</NavLink></li>
-      <li><NavLink to="/fashion">Fashion</NavLink></li>
-    </ul>
-  </nav>
-);
+const TopNav: React.FC = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      <button className="nav-toggle" onClick={() => setOpen(!open)}>
+        a•th•eh
+      </button>
+      {open && (
+        <>
+          <div className="nav-backdrop" onClick={() => setOpen(false)} />
+          <nav className="nav-overlay">
+          <ul className="nav-links">
+            <li><NavLink to="/" onClick={() => setOpen(false)}>Home</NavLink></li>
+            <li><NavLink to="/cafe" onClick={() => setOpen(false)}>Cafe</NavLink></li>
+            <li><NavLink to="/cooking" onClick={() => setOpen(false)}>Cooking</NavLink></li>
+            <li><NavLink to="/travel" onClick={() => setOpen(false)}>Travel</NavLink></li>
+            <li><NavLink to="/fashion" onClick={() => setOpen(false)}>Fashion</NavLink></li>
+          </ul>
+        </nav>
+        </>
+      )}
+    </>
+  );
+};
 
 export default TopNav;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 
-const text = 'EVERYDAY ATHE ';
+const spiralText = 'EVERYDAY ATHE ';
 
 const HomePage: React.FC = () => (
   <div className="home">
-    <div className="marquee">
-      <span>{text.repeat(10)}</span>
+    <div className="spiral">
+      {spiralText.split('').map((char, i) => (
+        <span key={i} style={{ transform: `rotate(${i * (360 / spiralText.length)}deg) translateY(-120px)` }}>
+          {char}
+        </span>
+      ))}
     </div>
-    <div className="marquee reverse">
-      <span>{text.repeat(10)}</span>
-    </div>
-    <div className="marquee">
-      <span>{text.repeat(10)}</span>
+    <div className="home-images">
+      <img src="https://res.cloudinary.com/dlenbkeui/image/upload/v1769138292/Scanned_Document-1_mopo3y.png" alt="" />
+      <img src="https://res.cloudinary.com/dlenbkeui/image/upload/v1769138298/Scanned_Document-6_y9ldfp.png" alt="" />
     </div>
   </div>
 );
